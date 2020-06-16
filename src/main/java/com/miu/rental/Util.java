ee9d80ba9b894fab63e13f5cbdae49e687595e28
Grace <bernietes0@gmail.com>
2020-06-16 02:29:54 -0500

    User Registration

--- src/main/java/com/miu/rental/Util.java
+++ src/main/java/com/miu/rental/Util.java
@@ -13,21 +13,40 @@ public class Util {
     }
 
     public static boolean isLoggedIn(HttpServletRequest req) {
-     if (req.getSession().getAttribute("userID")!=null){
-         int userID= (int) req.getSession().getAttribute("userID");
-         return UserRepository.getById(userID) != null;
-     }
-     return false;
+        if (req.getSession().getAttribute("userID") != null) {
+            int userID = (int) req.getSession().getAttribute("userID");
+            return UserRepository.getById(userID) != null;
+        }
+        return false;
     }
 
     public static boolean isAdmin(HttpServletRequest req) {
-        if (req.getSession().getAttribute("userID")!=null){
-            int userID= (int) req.getSession().getAttribute("userID");
+        if (req.getSession().getAttribute("userID") != null) {
+            int userID = (int) req.getSession().getAttribute("userID");
             User user = UserRepository.getById(userID);
             assert user != null;
-            return user.getRole()== Role.ADMIN;
+            return user.getRole() == Role.ADMIN;
         }
         return false;
+    }
+
+    public static String registrationValidation(HttpServletRequest req) {
+        String firstName = req.getParameter("inputFirstName");
+        String lastName = req.getParameter("inputLastName");
+        String userName = req.getParameter("inputUserName");
+        String email = req.getParameter("inputEmailAddress");
+        String password = req.getParameter("inputPassword");
+        String conformPassword = req.getParameter("inputConfirmPassword");
+        String error_message = "";
+        if (firstName.equals("") || lastName.equals("") || userName.equals("") || email.equals("") || password.equals("") || conformPassword.equals(""))
+            error_message += " |*All Field are required";
+
+        if (!password.equals(conformPassword))
+            error_message += " |*Password didn't match";
+
+        if (UserRepository.getByUserName(userName) != null)
+            error_message += " |*user name exits ";
+        return error_message;
     }
 
 
--- src/main/java/com/miu/rental/controller/UserRegistryController.java
+++ src/main/java/com/miu/rental/controller/UserRegistryController.java
@@ -1,5 +1,9 @@
 package com.miu.rental.controller;
 
+import com.miu.rental.Util;
+import com.miu.rental.dao.UserRepository;
+import com.miu.rental.model.customAttributeType.Role;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,7 +16,37 @@ import java.io.IOException;
 public class UserRegistryController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher view = req.getRequestDispatcher("/WEB-INF/register.jsp");
-        view.forward(req,resp);
+        if (!Util.isLoggedIn(req)) {
+            RequestDispatcher view = req.getRequestDispatcher("/WEB-INF/register.jsp");
+            view.forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String error_message = Util.registrationValidation(req);
+        if (error_message.equals("")) {
+            String firstName = req.getParameter("inputFirstName");
+            String lastName = req.getParameter("inputLastName");
+            String userName = req.getParameter("inputUserName");
+            String email = req.getParameter("inputEmailAddress");
+            String password = req.getParameter("inputPassword");
+            String conformPassword = req.getParameter("inputConfirmPassword");
+            UserRepository.addUser(firstName, lastName, userName, password, email, Role.CLIENT);
+            req.setAttribute("inputUserName",userName);
+            req.setAttribute("inputPassword",password);
+            RequestDispatcher view = req.getRequestDispatcher("/login");
+            view.forward(req, resp);
+
+
+        } else {
+            req.setAttribute("error_message", error_message);
+
+            RequestDispatcher view = req.getRequestDispatcher("/WEB-INF/register.jsp");
+            view.forward(req, resp);
+
+        }
+
+
     }
 }
