4b1d1a3e49adbbe3d3c7fb9e7bdc5dfbc32cfbe0
Grace <bernietes0@gmail.com>
2020-06-17 13:05:48 -0500

    Log out Url bug

--- src/main/java/com/miu/rental/controller/dashboard/LogoutController.java
+++ src/main/java/com/miu/rental/controller/dashboard/LogoutController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/logout")
+@WebServlet({"/logout","/admin/logout","/user/logout"})
 public class LogoutController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
